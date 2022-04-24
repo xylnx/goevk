b047d9ab92381352c3adb0aeba83e410af976b13
xylnx <xylnx@mailbox.org>
2022-04-24 13:13:41 +0200

    Wrap the whole event card in a link

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,32 +1,20 @@
 export const Event = ({ event }) => {
   return (
     <article className="event-card" data-color={event.dateDetails.weekday}>
-      <div className="event-card__upper">
-        <div className="event__place">{event.place}</div>
-        <div className="event__date">{event.dateDetails.date}</div>
-        <div className="event__day">[ {event.dateDetails.weekday} ]</div>
-        <div className="event__type">{event.type}</div>
-      </div>
-      <div className="event-card__lower">
-        <div className="event__time">{event.dateDetails.time}</div>
+      <a href={event.link} target="blank" rel="noopener">
+        <div className="event-card__upper">
+          <div className="event__place">{event.place}</div>
+          <div className="event__date">{event.dateDetails.date}</div>
+          <div className="event__day">[ {event.dateDetails.weekday} ]</div>
+          <div className="event__type">{event.type}</div>
+        </div>
+        <div className="event-card__lower">
+          <div className="event__time">{event.dateDetails.time}</div>
 
-        {/* Title Link */}
-        <a
-          href={event.link}
-          target="blank"
-          rel="noopener"
-          className="event__name"
-        >
-          {event.name}
-        </a>
+          {/* Title */}
+          <div className="event__name">{event.name}</div>
 
-        {/* Arrow Link */}
-        <a
-          href={event.link}
-          target="blank"
-          rel="noopener"
-          className="event__link"
-        >
+          {/*Arrow */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -36,8 +24,8 @@ export const Event = ({ event }) => {
           >
             <path d="m9.77 12.11 4.012-2.953a.647.647 0 0 0 0-1.114L9.771 5.09a.644.644 0 0 0-.971.557V6.65H2v3.9h6.8v1.003c0 .505.545.808.97.557z" />
           </svg>
-        </a>
-      </div>
+        </div>
+      </a>
     </article>
   );
 };
