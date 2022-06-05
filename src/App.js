ae70a36dd21452cf43ab40d9c2fcedd7418014bc
xylnx <xylnx@mailbox.org>
2022-06-05 17:17:18 +0200

    Make route properties available to components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import Header from "./components/Header";
-import { ButtonNav } from "./components/ButtonNav";
-import { EventList } from "./components/EventList";
+import Header from './components/Header';
+import { ButtonNav } from './components/ButtonNav';
+import { EventList } from './components/EventList';
 
 import {
   //BrowserRouter as Router,
   HashRouter,
   Route,
   Switch,
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import { useTheme } from "./hooks/useTheme";
+import { useTheme } from './hooks/useTheme';
 
-import { GlobalStyles, GlobalStylesLight } from "./styles/GlobalStyles";
+// Use global styles to change themes
+import { GlobalStyles, GlobalStylesLight } from './styles/GlobalStyles';
 
-// Filters
-import { FilterToday, FilterTomorrow, FilterAll } from "./filters/FilterDates";
+// Date filters
+import { FilterToday, FilterTomorrow, FilterAll } from './filters/FilterDates';
 
 function App() {
   const { mode } = useTheme();
   return (
     <HashRouter>
-      {mode === "dark" && <GlobalStyles />}
-      {mode === "light" && <GlobalStylesLight />}
+      {mode === 'dark' && <GlobalStyles />}
+      {mode === 'light' && <GlobalStylesLight />}
       <div className="App">
         <Header />
         <ButtonNav />
         <main className="main">
           <Switch>
-            <Route exact path="/">
-              <EventList filter={FilterToday} />
-            </Route>
-            <Route exact path="/tomorrow">
-              <EventList filter={FilterTomorrow} />
-            </Route>
-            <Route exact path="/all">
-              <EventList filter={FilterAll} />
-            </Route>
+            <Route
+              exact
+              path="/"
+              render={(routeProps) => (
+                <EventList filter={FilterToday} routeProps={routeProps} />
+              )}
+            ></Route>
+            <Route
+              exact
+              path="/tomorrow"
+              render={(routeProps) => (
+                <EventList filter={FilterTomorrow} routeProps={routeProps} />
+              )}
+            ></Route>
+            <Route
+              exact
+              path="/all"
+              render={(routeProps) => (
+                <EventList filter={FilterAll} routeProps={routeProps} />
+              )}
+            />
           </Switch>
         </main>
       </div>
