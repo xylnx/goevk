e99ab4f47e82d0e8246b10dd32324878c925dda6
xylnx <xylnx@mailbox.org>
2022-06-05 17:18:47 +0200

    Change markup to better work with event type sorting feature

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,29 +1,46 @@
+import { Link } from 'react-router-dom';
 import { IconArrow } from '../assets/icon-arrow';
 
-export const Event = ({ event }) => {
+const typeLink = (eventType) => {
+  const types = eventType.split(',');
+  let query = '?';
+  let params = 'type=';
+  params += types.reduce((acc, cur) => {
+    return acc + `&type=${cur.trim()}`;
+  });
+
+  query += params;
+  return query;
+};
+
+export const Event = ({ event, slug }) => {
+  // console.log({ slug });
   return (
     <article className="event-card" data-color={event.dateDetails.weekday}>
-      {/* Event meta data */}
-      <div className="event-card__upper">
-        <div className="event__place">{event.place}</div>
-        <div className="event__date">{event.dateDetails.date}</div>
-        <div className="event__day">[ {event.dateDetails.weekday} ]</div>
-        <div className="event__type">{event.type}</div>
-      </div>
+      <a href={event.link} target="blank" rel="noopener">
+        {/* Event meta data */}
+        <div className="event-card__upper">
+          <div className="event__place">{event.place}</div>
+          <div className="event__date">{event.dateDetails.date}</div>
+          <div className="event__day">[ {event.dateDetails.weekday} ]</div>
+          <Link
+            to={`${slug ? slug : ''}${typeLink(event.type)}`}
+            className="event__type"
+          >
+            {event.type}
+          </Link>
+        </div>
 
-      <div className="event-card__lower">
-        <div className="event__time">{event.dateDetails.time}</div>
+        <div className="event-card__lower">
+          <div className="event__time">{event.dateDetails.time}</div>
 
-        {/* Event name */}
-        <a href={event.link} target="blank" rel="noopener">
+          {/* Event name */}
           <div className="event__name">{event.name}</div>
-        </a>
 
-        {/* Arrow */}
-        <a href={event.link} target="blank" rel="noopener">
+          {/* Arrow */}
           <IconArrow />
-        </a>
-      </div>
+        </div>
+      </a>
     </article>
   );
 };
