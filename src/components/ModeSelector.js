60c543e01977126748e902123c6388759ff58888
xylnx <xylnx@mailbox.org>
2022-06-20 07:00:48 +0200

    Remove `console.log` + formatting changes (prettier config)

--- src/components/ModeSelector.js
+++ src/components/ModeSelector.js
@@ -1,22 +1,21 @@
 // Custom hooks
-import { useTheme } from "../hooks/useTheme";
+import { useTheme } from '../hooks/useTheme';
 
 // Components
-import { IconModeLight, IconModeDark } from "../assets/icon-mode";
+import { IconModeLight, IconModeDark } from '../assets/icon-mode';
 
 export const ModeSelector = ({ cssClass }) => {
   const { mode, changeMode } = useTheme();
-  console.log({ mode });
   return (
     <div className={cssClass}>
       <button
         className={`${cssClass}__button`}
         onClick={() =>
-          mode === "dark" ? changeMode("light") : changeMode("dark")
+          mode === 'dark' ? changeMode('light') : changeMode('dark')
         }
       >
-        {mode === "light" && <IconModeLight />}
-        {mode === "dark" && <IconModeDark />}
+        {mode === 'light' && <IconModeLight />}
+        {mode === 'dark' && <IconModeDark />}
       </button>
     </div>
   );
