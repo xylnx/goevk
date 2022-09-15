ba115145a11c95dd5d926121a2d297e0e0ab5c98
xylnx <xylnx@mailbox.org>
2022-09-15 12:15:42 +0200

    Simplify event card markup

--- src/components/Event.js
+++ src/components/Event.js
@@ -17,28 +17,33 @@ export const Event = ({ event, slug }) => {
   return (
     <article className="event-card" data-color={event.dateDetails.weekday}>
       {/* Event meta data */}
-      <div className="event-card__upper">
-        <div className="event__place">{event.place}</div>
-        <div className="event__date">{event.dateDetails.date}</div>
-        <div className="event__day">[ {event.dateDetails.weekday} ]</div>
-        <Link
-          to={`${slug ? slug : ''}${typeLink(event.type)}`}
-          className="event__type"
-        >
-          {event.type}
-        </Link>
+      <div className="event__place">{event.place}</div>
+      <div className="event__date">
+        {event.dateDetails.date}
+        <span className="event__day">[{event.dateDetails.weekday}]</span>
       </div>
 
-      <a href={event.link} target="blank" rel="noopener">
-        <div className="event-card__lower">
-          <div className="event__time">{event.dateDetails.time}</div>
+      {/* Categories to sort events */}
+      <Link
+        to={`${slug ? slug : ''}${typeLink(event.type)}`}
+        className="event__type"
+      >
+        {event.type}
+      </Link>
 
-          {/* Event name */}
-          <div className="event__name">{event.name}</div>
+      <div className="event__time">{event.dateDetails.time}</div>
 
-          {/* Arrow */}
-          <IconArrow />
-        </div>
+      {/* Event name, links a page related to the event */}
+      <a
+        href={event.link}
+        target="blank"
+        rel="noopener"
+        className="event__name"
+      >
+        {event.name}
+
+        {/* Arrow */}
+        <IconArrow />
       </a>
     </article>
   );
