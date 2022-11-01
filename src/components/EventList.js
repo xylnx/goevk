15e6402347a7d1df96d0f90398ade4a274915a30
xylnx <xylnx@mailbox.org>
2022-11-01 10:57:19 +0100

    Update api endpoint URL

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -13,9 +13,8 @@ import { Event } from './Event';
 import EventsNone from './EventsNone';
 
 // API ENDPOINTS
+const apiEndpoint = 'https://sleepy-crag-13951.herokuapp.com/bvents.json';
 const localEndpoint = 'http://localhost:5033/events.json';
-// const localEndpoint = 'http://localhost:3000/events';
-const apiEndpoint = 'https://sleepy-crag-13951.herokuapp.com/events.json';
 
 export const EventList = ({ filter }) => {
   const url =
